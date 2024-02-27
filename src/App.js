5c4741c64bd44e1fa1a0cd4c140adf7becedf30e
jiglesias2000 <jiglesias.2000@gmail.com>
2024-02-27 20:37:02 -0300

    etapa 3 completa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { Inicio } from "./components/Inicio";
+import { ArticulosFamilias } from "./components/ArticulosFamilias";
+import { Menu } from "./components/Menu";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Menu />
+        <div className="divBody">
+            <Routes>
+              <Route path="/inicio" element={<Inicio />} />
+              <Route path="/articulosfamilias" element={<ArticulosFamilias />} />
+              <Route path="*" element={<Navigate to="/Inicio" replace />} />
+            </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
-
 export default App;
