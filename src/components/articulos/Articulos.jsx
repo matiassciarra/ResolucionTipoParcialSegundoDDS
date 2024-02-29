ab55317d64f4567f323d30a7790a1961367dda5d
jiglesias2000 <jiglesias.2000@gmail.com>
2024-02-29 19:10:03 -0300

    etapa 5 completa

--- src/components/articulos/Articulos.jsx
+++ src/components/articulos/Articulos.jsx
@@ -3,7 +3,10 @@ import moment from "moment";
 import ArticulosBuscar from "./ArticulosBuscar";
 import ArticulosListado from "./ArticulosListado";
 import ArticulosRegistro from "./ArticulosRegistro";
-import { articulosFamiliasMockService as articulosfamiliasService } from "../../services/articulosFamilias-mock-service";
+import { articulosService } from "../../services/articulos.service";
+import { articulosfamiliasService } from "../../services/articulosFamilias.service";
+//import { articulosFamiliasMockService as articulosfamiliasService } from "../../services/articulosFamilias-mock-service";
+
 
 function Articulos() {
   const TituloAccionABMC = {
@@ -35,44 +38,33 @@ function Articulos() {
     BuscarArticulosFamilas();
   }, []);
 
-  async function Buscar() {
-    setAccionABMC("L");
-    // harcodeamos 2 articulos para probar
-    setItems([
-      {
-        IdArticulo: 108,
-        Nombre: "Adaptador usb wifi tl-wn722n",
-        Precio: 219.0,
-        CodigoDeBarra: "0693536405046",
-        IdArticuloFamilia: 9,
-        Stock: 898,
-        FechaAlta: "2017-01-23T00:00:00",
-        Activo: false,
-      },
-      {
-        IdArticulo: 139,
-        Nombre: "Aire acondicionado daewoo 3200fc dwt23200fc",
-        Precio: 5899.0,
-        CodigoDeBarra: "0779816944014",
-        IdArticuloFamilia: 7,
-        Stock: 668,
-        FechaAlta: "2017-01-04T00:00:00",
-        Activo: true,
-      },
-    ]);
-    alert("Buscando...");
+  async function Buscar(_pagina) {
+    if (_pagina && _pagina !== Pagina) {
+      setPagina(_pagina);
+    }
+    // OJO Pagina (y cualquier estado...) se actualiza para el proximo render, para buscar usamos el parametro _pagina
+    else {
+      _pagina = Pagina;
+    }
+
+    const data = await articulosService.Buscar(Nombre, Activo, _pagina);
+    setItems(data.Items);
+    setRegistrosTotal(data.RegistrosTotal);
+
+    //generar array de las páginas para mostrar en select del paginador
+    const arrPaginas = [];
+    for (let i = 1; i <= Math.ceil(data.RegistrosTotal / 10); i++) {
+      arrPaginas.push(i);
+    }
+    setPaginas(arrPaginas);
   }
 
   async function BuscarPorId(item, accionABMC) {
+    const data = await articulosService.BuscarPorId(item);
+    setItem(data);
     setAccionABMC(accionABMC);
-    setItem(item);
-    if (accionABMC === "C") {
-      alert("Consultando...");
-    }
-    if (accionABMC === "M") {
-      alert("Modificando...");
-    }
   }
+  
 
   function Consultar(item) {
     BuscarPorId(item, "C"); // paso la accionABMC pq es asincrono la busqueda y luego de ejecutarse quiero cambiar el estado accionABMC
@@ -98,7 +90,6 @@ function Articulos() {
         Activo: true,
       });
     alert("preparando el Alta...");
-
   }
 
   function Imprimir() {
@@ -112,19 +103,36 @@ function Articulos() {
         " el registro?"
     );
     if (resp) {
-      alert("Activando/Desactivando...");
+      await articulosService.ActivarDesactivar(item);
+      await Buscar();
     }
   }
+  
+  
 
   async function Grabar(item) {
-    alert(
-      "Registro " +
-        (AccionABMC === "A" ? "agregado" : "modificado") +
-        " correctamente."
-    );
-
+    // agregar o modificar
+    try
+    {
+      await articulosService.Grabar(item);
+    }
+    catch (error)
+    {
+      alert(error?.response?.data?.message ?? error.toString())
+      return;
+    }
+    await Buscar();
     Volver();
+  
+    setTimeout(() => {
+      alert(
+        "Registro " +
+          (AccionABMC === "A" ? "agregado" : "modificado") +
+          " correctamente."
+      );
+    }, 0);
   }
+  
 
   // Volver/Cancelar desde Agregar/Modificar/Consultar
   function Volver() {
