14570c609f3addd9b36e7b8f08158ae81e3a04d5
jiglesias2000 <jiglesias.2000@gmail.com>
2024-03-04 22:00:12 -0300

    ajustes titulo

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 //const urlServidor = "http://localhost:3000"
+//const urlServidor = ""  // vacio para cuando se despliega el frontend, en el mismo servidor que el backend
 const urlServidor = "https://dds-backend.azurewebsites.net"
 //const urlServidor = "https://webapi.pymes.net.ar"
 //const urlServidor = "https://labsys.frc.utn.edu.ar/dds-express"
